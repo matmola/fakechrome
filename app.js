e4901fca8554ee66814fb04ad193a068abfdd3ec
matmola <jinii20@naver.com>
2022-02-04 14:36:06 +0900

    javascript basic

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+
+// array
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
