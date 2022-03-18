a125ba4dd5fbec795277585158a5989a0398e7f3
matmola <jinii20@naver.com>
2022-03-18 17:25:01 +0900

    css

--- js/background.js
+++ js/background.js
@@ -1,8 +1,7 @@
 const images = ["zelda1.jpg","zelda2.jpg","zelda3.jpg"];
-
+const body = document.querySelector("body");
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
 const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;  //난수를 생성해 반올림 시켜서 image 어레이 속 사진 하나를 가져옴
 
+bgImage.src = `img/${chosenImage}`;  //난수를 생성해 반올림 시켜서 image 어레이 속 사진 하나를 가져옴
 document.body.appendChild(bgImage); //body에 html 추가
